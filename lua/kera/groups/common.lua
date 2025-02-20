2f569e8d6fc9150fefa1ff7699e92a2414b9c1d7
Adrian Najera <39295224+adriangitvitz@users.noreply.github.com>
2025-02-20 05:57:23 -0600

    Change color lsp

--- lua/kera/groups/common.lua
+++ lua/kera/groups/common.lua
@@ -33,7 +33,7 @@ M.get_colors = function(conf)
         Directory        = { fg = p.color300, bg = "none" },
         ErrorMsg         = { fg = p.error, style = 'bold' },
         EndOfBuffer      = { fg = p.color400 or p.color600 },
-        FloatBorder      = { bg = "#101421", fg = "#101421" }, -- Hover border
+        FloatBorder      = { bg = "#101421", fg = p.color200 }, -- Hover border
         FloatTitle       = { fg = p.builtin, bold = true, bg = "none" },
         FloatFooter      = { fg = p.color100, bg = "none" },
         Folded           = { fg = p.color200, bg = "none" }, -- TODO: Change none to panel color
@@ -42,7 +42,7 @@ M.get_colors = function(conf)
         LineNr           = { fg = p.comment, bg = "#0E1019" },
         MatchParen       = { fg = p.color200, bg = p.color700 },
         MoreMsg          = { fg = p.color200, gui = "bold" },
-        MsgSeparator     = { fg = p.builtin, bg = p.placeholder, gui = "bold" },
+        MsgSeparator     = { fg = "#101421", bg = "NONE", gui = "bold" },
         NonText          = { fg = p.color500 },
         Normal           = { fg = p.fg, bg = "none" },
         NormalNC         = { fg = p.color200, bg = "#0E1019" },
