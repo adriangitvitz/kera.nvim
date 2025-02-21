07cb46bc15a53b3a6e0e9861ab8a209fe5831ccb
Adrian Najera <39295224+adriangitvitz@users.noreply.github.com>
2025-02-20 23:32:50 -0600

    Telescope changes

--- lua/kera/groups/telescope.lua
+++ lua/kera/groups/telescope.lua
@@ -9,12 +9,12 @@ M.get_colors = function(conf)
         TelescopeBorder            = { fg = p.color100 },                                    -- Border for Telescope
         TelescopeMatching          = { fg = p.primary, gui = conf.plugins.telescope.match }, -- Matching text in Telescope
         TelescopePromptPrefix      = { fg = p.color200 },                                    -- Prefix in Telescope prompt
-        TelescopeSelection         = { fg = p.constant, bg = p.line },                       -- Selection in Telescope
+        TelescopeSelection         = { fg = p.constant, bg = "NONE" },                       -- Selection in Telescope
         TelescopeSelectionCaret    = { fg = p.constant },                                    -- Caret in Telescope selection
         TelescopeResultsNormal     = { fg = p.fg },                                          -- Normal text in Telescope results
         TelescopeNormal            = { fg = p.color200 },
         -- TelescopeSelection         = { fg = p.color200, bg = p.color700 },
-        TelescopeTitle             = { fg = p.color200 },
+        TelescopeTitle             = { fg = p.primary },
         TelescopePromptBorder      = { fg = p.color700 },
         TelescopePromptNormal      = { fg = p.color100 },
         TelescopePreviewTitle      = { fg = p.color100, bg = p.color700 },
